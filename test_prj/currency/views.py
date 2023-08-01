8a9bed3911dbccfc9cd151fb541b6390a8ae96d1
bochok26 <o.bochok26@gmail.com>
2023-08-01 16:50:37 +0300

    updated render & download as pdf using pdfkit

--- test_prj/currency/views.py
+++ test_prj/currency/views.py
@@ -2,20 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.template.loader import render_to_string
-import io
-from django.http import FileResponse
-from reportlab.pdfgen import canvas
+import pdfkit, os
 import urllib.request, json
 
-from xhtml2pdf import pisa
-# from django.template import Context
-
-# Create your views here.
 
 def get_json():
     with urllib.request.urlopen("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20210101&end=20211231&valcode=usd&sort=exchangedate&order=asc&json") as url:
         data = json.loads(url.read().decode('utf-8'))
     return data
+
 
 def currency(request):
     template = loader.get_template('page.html')
@@ -25,39 +20,20 @@ def currency(request):
     }
     return HttpResponse(template.render(context))
     
-"""def to_pdf(request):
-    buffer = io.BytesIO()
 
-    p = canvas.Canvas(buffer)
-    data = get_json()
-
-    p.drawString("".join(data))
-
-    p.showPage()
-    p.save()
-
-    buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True)"""
-    
-def render_pdf(template_src, context_dict):
-    template = loader.get_template(template_src)
-    #context = Context(context_dict)
-    #html  = template.render(context)
-    html = render_to_string(template_src, context_dict)
-    result = io.BytesIO()
-
-    pdf = pisa.pisaDocument(io.BytesIO(html.encode("utf-8")), result)
-    return HttpResponse(result.getvalue(), content_type='application/pdf')
-    
 def to_pdf(request):
     results = get_json()
-    return render_pdf(
-            'layout.html',
-            {
-                'pagesize':'A4',
-                'data': results,
-            }
-        )
+    template = loader.get_template("layout.html")
+    context = {"data": results, }  # data is the context data that is sent to the html file to render the output. 
+    html = template.render(context)  # Renders the template with the context data.
+    pdfkit.from_string(html, 'currency.pdf')
+    pdf = open("currency.pdf", 'rb')
+    response = HttpResponse(pdf.read(), content_type='application/pdf')  # Generates the response as pdf response.
+    response['Content-Disposition'] = 'attachment; filename=currency.pdf'
+    pdf.close()
+    os.remove("currency.pdf")  # remove the locally created pdf file.
+    return response
+
 
 def main(request):
   template = loader.get_template('main.html')
